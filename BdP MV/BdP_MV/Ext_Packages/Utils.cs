11b56bf27fafc5d2b7199c7510e7b4e8155800fc
Atddo <github@atddo.de>
2018-01-16 08:51:29 +0100

    Things

---
+++ BdP MV/BdP_MV/Ext_Packages/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MvvmHelpers
+{
+    /// <summary>
+    /// Extension Utils
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Task extension to add a timeout.
+        /// </summary>
+        /// <returns>The task with timeout.</returns>
+        /// <param name="task">Task.</param>
+        /// <param name="timeoutInMilliseconds">Timeout duration in Milliseconds.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public async static Task<T> WithTimeout<T>(this Task<T> task, int timeoutInMilliseconds)
+        {
+            var retTask = await Task.WhenAny(task, Task.Delay(timeoutInMilliseconds))
+                .ConfigureAwait(false);
+
+            return retTask is Task<T> ? task.Result : default(T);
+        }
+
+        /// <summary>
+        /// Task extension to add a timeout.
+        /// </summary>
+        /// <returns>The task with timeout.</returns>
+        /// <param name="task">Task.</param>
+        /// <param name="timeout">Timeout Duration.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static Task<T> WithTimeout<T>(this Task<T> task, TimeSpan timeout) =>
+            WithTimeout(task, (int)timeout.TotalMilliseconds);
+        
+    }
+}
+
